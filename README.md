6182979bda570219887d535cc7b17fe1c04909cf
Tim Hordern <tim.hordern@gmail.com>
2021-06-06 19:14:06 +1000

    Update README and configuration script

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Windows Workstation Builder
 
-Scripts to build my Windows workstation
+Scripts to build my Windows workstation. Uses [Boxstarter](https://boxstarter.org/) and [Chocolately](https://chocolatey.org/).
 
 1. Install Boxstarter: `Install-Boxstarter.ps1`
 2. Prepare system environment for installs: `Setup-Install-Environment.ps1`
 3. Remove any bloatware applications already installed: `Remove-Default-Apps.ps1`
-4. Configure system: `System-Configuration.ps1`
-5. Install software: `Install-Software.ps1`
-6. Cleanup from install: `Cleanup.ps1`
+4. Disable unwanted Windows Services: `Disable-Services.ps1`
+5. Disable Windows telemetry via HOSTS or Firewall blocking: `Disable-Telemetry.ps1`
+6. Configure system: `System-Configuration.ps1`
+7. Install software: `Install-Software.ps1`
+8. Cleanup from install: `Cleanup.ps1`
 
-## TODO: Call Setup through to Cleanup from a single calling script
+TODO: Call Setup through to Cleanup from a single calling script
--- System-Configuration.ps1
+++ System-Configuration.ps1
@@ -85,3 +85,137 @@ Function DisableAppSuggestions {
   }
 }
 DisableAppSuggestions
+
+Write-Output "=== Fix privacy settings ==="
+
+# https://github.com/W4RH4WK/Debloat-Windows-10/blob/master/scripts/optimize-user-interface.ps1
+Write-Output "Elevating priviledges for this process"
+do {} until (Elevate-Privileges SeTakeOwnershipPrivilege)
+
+Write-Output "--- Disable Game DVR and Game Bar ---"
+New-FolderForced -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR" "AllowgameDVR" 0
+
+Write-Output "--- Disable easy access keyboard stuff ---"
+Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\StickyKeys" "Flags" "506"
+Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\Keyboard Response" "Flags" "122"
+Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\ToggleKeys" "Flags" "58"
+
+Write-Output "--- Disable Edge desktop shortcut on new profiles ---"
+New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer" -Name DisableEdgeDesktopShortcutCreation -PropertyType DWORD -Value 1
+
+Write-Output "--- Disable Aero-Shake Minimize feature ---"
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "DisallowShaking" 1
+
+Write-Output "--- Disabling telemetry via Group Policies ---"
+New-FolderForced -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" "AllowTelemetry" 0
+
+# https://github.com/W4RH4WK/Debloat-Windows-10/blob/master/scripts/fix-privacy-settings.ps1
+Write-Output "--- Set general privacy options ---"
+Write-Output "--- Disabling Let websites provide locally relevant content by accessing my language list ---"
+Set-ItemProperty -Path "HKCU:\Control Panel\International\User Profile" "HttpAcceptLanguageOptOut" 1
+Write-Output "--- Disabling Locaton aware printing (changes default based on connected network)"
+New-FolderForced -Path "HKCU:\Printers\Defaults"
+Set-ItemProperty -Path "HKCU:\Printers\Defaults" "NetID" "{00000000-0000-0000-0000-000000000000}"
+Write-Output "--- Disabling Send Microsoft info about how I write to help us improve typing and writing in the future ---"
+New-FolderForced -Path "HKCU:\SOFTWARE\Microsoft\Input\TIPC"
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Input\TIPC" "Enabled" 0
+Write-Output "--- Disabling Let apps use my advertising ID for experiencess across apps ---"
+New-FolderForced -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo"
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo" "Enabled" 0
+Write-Output "--- Disabling SmartScreen Filter to check web content ---"
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppHost" "EnableWebContentEvaluation" 0
+
+Write-Output "--- Disable synchronisation of settings ---"
+# These only apply if you log on using Microsoft account
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\SettingSync" "BackupPolicy" 0x3c
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\SettingSync" "DeviceMetadataUploaded" 0
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\SettingSync" "PriorLogons" 1
+$groups = @(
+    "Accessibility"
+    "AppSync"
+    "BrowserSettings"
+    "Credentials"
+    "DesktopTheme"
+    "Language"
+    "PackageState"
+    "Personalization"
+    "StartLayout"
+    "Windows"
+)
+foreach ($group in $groups) {
+    New-FolderForced -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\SettingSync\Groups\$group"
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\SettingSync\Groups\$group" "Enabled" 0
+}
+
+Write-Output "--- Set personalization privacy policy accepted state to 0 ---"
+# Prevents sending speech, inking and typing samples to MS (so Cortana
+# can learn to recognise you)
+New-FolderForced -Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings"
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings" "AcceptedPrivacyPolicy" 0
+
+Write-Output "--- Do not scan contact information ---"
+# Prevents sending contacts to MS (so Cortana can compare speech etc samples)
+New-FolderForced -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore"
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore" "HarvestContacts" 0
+
+Write-Output "--- Restrict handwriting recognition personalization ---"
+# Handwriting recognition personalization
+New-FolderForced -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization"
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" "RestrictImplicitInkCollection" 1
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" "RestrictImplicitTextCollection" 1
+
+Write-Output "--- Set Microsoft Edge settings ---"
+New-FolderForced -Path "HKCU:\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main"
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main" "DoNotTrack" 1
+New-FolderForced -Path "HKCU:\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\User\Default\SearchScopes"
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\User\Default\SearchScopes" "ShowSearchSuggestionsGlobal" 0
+New-FolderForced -Path "HKCU:\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\FlipAhead"
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\FlipAhead" "FPEnabled" 0
+New-FolderForced -Path "HKCU:\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\PhishingFilter"
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\PhishingFilter" "EnabledV9" 0
+
+Write-Output "--- Disable background access of default apps ---"
+foreach ($key in (Get-ChildItem "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications")) {
+    Set-ItemProperty -Path ("HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications\" + $key.PSChildName) "Disabled" 1
+}
+
+Write-Output "--- Disable sharing information with unpaired devices ---"
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\LooselyCoupled" "Type" "LooselyCoupled"
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\LooselyCoupled" "Value" "Deny"
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\LooselyCoupled" "InitialAppValue" "Unspecified"
+foreach ($key in (Get-ChildItem "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global")) {
+    if ($key.PSChildName -EQ "LooselyCoupled") {
+        continue
+    }
+    Set-ItemProperty -Path ("HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\" + $key.PSChildName) "Type" "InterfaceClass"
+    Set-ItemProperty -Path ("HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\" + $key.PSChildName) "Value" "Deny"
+    Set-ItemProperty -Path ("HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\" + $key.PSChildName) "InitialAppValue" "Unspecified"
+}
+
+Write-Output "--- Disable submission of Windows Defender findings (w/ elevated privileges) ---"
+Takeown-Registry("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender\Spynet")
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows Defender\Spynet" "SpyNetReporting" 0       # write-protected even after takeown ?!
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows Defender\Spynet" "SubmitSamplesConsent" 0
+
+Write-Output "=== Optimize Windows Update ==="
+
+# https://github.com/W4RH4WK/Debloat-Windows-10/blob/master/scripts/optimize-windows-update.ps1
+
+Write-Output "--- Disable automatic download and installation of Windows updates ---"
+New-FolderForced -Path "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\WindowsUpdate\AU"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\WindowsUpdate\AU" "NoAutoUpdate" 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\WindowsUpdate\AU" "AUOptions" 2
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\WindowsUpdate\AU" "ScheduledInstallDay" 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\WindowsUpdate\AU" "ScheduledInstallTime" 3
+
+Write-Output "--- Disable seeding of updates to other computers via Group Policies ---"
+New-FolderForced -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization" "DODownloadMode" 0
+
+Write-Output "--- Disable Updates are available message ---"
+takeown /F "$env:WinDIR\System32\MusNotification.exe"
+icacls "$env:WinDIR\System32\MusNotification.exe" /deny "$($EveryOne):(X)"
+takeown /F "$env:WinDIR\System32\MusNotificationUx.exe"
+icacls "$env:WinDIR\System32\MusNotificationUx.exe" /deny "$($EveryOne):(X)"
